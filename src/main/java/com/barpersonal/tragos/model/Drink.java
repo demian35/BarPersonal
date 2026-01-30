e615f33f2eab71c8bddfc4f63839dc960535da2f
Demian <josedemian@ciencias.unam.mx>
2026-01-29 21:58:48 -0600

    agregando ingredientes del cocktel

--- src/main/java/com/barpersonal/tragos/model/Drink.java
+++ src/main/java/com/barpersonal/tragos/model/Drink.java
@@ -5,10 +5,18 @@ import lombok.Data;
 //clase drink que funcionara como dto para mapear el trago de la api
 @Data
 public class Drink {
+
+    //agregando atributos del coctel
     private String idDrink;
     private String strDrink;
     private String strDrinkThumb; // URL de la imagen
     private String strInstructions;
     private String strCategory;
+
+    // Aquí puedes agregar ingredientes si planeas ver el detalle del cocktail
+    private String strIngredient1;
+    private String strIngredient2;
+    private String strMeasure1;
+    private String strMeasure2;
     
 }
