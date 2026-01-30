990c7acfad6086e0104798e3e29d8fb340e4aa0d
Demian <josedemian@ciencias.unam.mx>
2026-01-29 21:38:32 -0600

    agregando estructura spring para el proyecto

---
+++ src/main/java/com/barpersonal/tragos/TragosApplication.java
@@ -0,0 +1,13 @@
+package com.barpersonal.tragos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TragosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TragosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/barpersonal/tragos/TragosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.barpersonal.tragos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TragosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
